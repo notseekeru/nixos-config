b8dec8a1361b89bec5300aa942e093b2f7700480
myteseeker18@gmail.com <myteseeker18@gmail.com>
2026-06-11 10:53:29 +0000

    feat: desktop package at system level

--- configuration.nix
+++ configuration.nix
@@ -60,7 +60,6 @@
       tree
       git
       gh
-      vim
     ];
   };
 
@@ -68,10 +67,17 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    tree
+    git
+    ghostty
+    waybar
+    wofi
+    networkmanagerapplet
+    dunst
+  ];
 
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
