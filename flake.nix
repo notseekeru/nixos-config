ea97cf6653b3f642971f248e79ddb3dfee8ae92f
myteseeker18@gmail.com <myteseeker18@gmail.com>
2026-06-12 10:02:21 +0800

    fix: syntax errors flake config

--- flake.nix
+++ flake.nix
@@ -1,10 +1,16 @@
+   {
+     inputs = {
+       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+     };
+
      outputs = { self, nixpkgs, ... }: {
        nixosConfigurations = {
          nixos = nixpkgs.lib.nixosSystem {
            system = "x86_64-linux";
            modules = [
-             ./configuration.nix # <-- This MUST be relative to the flake.nix
+             ./configuration.nix
            ];
          };
        };
      };
+   }
