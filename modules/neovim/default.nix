6bb29d3b04ef0a5307631468b2de74fbf3318af0
myteseeker18@gmail.com <myteseeker18@gmail.com>
2026-06-14 11:06:59 +0800

    fix: fixed harpoon addition

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -72,7 +72,7 @@
       local harpoon = require("harpoon")
       harpoon:setup({})
 
-      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
       vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Quick menu" })
       vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon: File 1" })
       vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon: File 2" })
