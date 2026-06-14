09304803c1f58ac1498a57be8c2bbc97ac1cf5c7
myteseeker18@gmail.com <myteseeker18@gmail.com>
2026-06-14 11:12:28 +0800

    chore: harpoon explorer remap

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -73,7 +73,7 @@
       harpoon:setup({})
 
       vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
-      vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Quick menu" })
+      vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Quick menu" })
       vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon: File 1" })
       vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon: File 2" })
       vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon: File 3" })
