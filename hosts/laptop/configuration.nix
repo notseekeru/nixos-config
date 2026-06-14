b1eb653c9d62e3808d5294f87016f422d8b26a2e
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-14 21:53:22 +0800

    feat: specifics conf of ssdm

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -11,8 +11,19 @@
   services.fstrim.enable = true;
 
   # SDDM display manager
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = false;  # Use X11 SDDM (more stable)
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = false;  # Use X11 SDDM (more stable)
+    settings = {
+      General = {
+        Numlock = "on";
+      };
+      Theme = {
+        ThemeDir = "/run/current-system/sw/share/sddm/themes";
+        Current = "maldives";
+      };
+    };
+  };
   services.displayManager.defaultSession = "hyprland";
 
   # Intel GPU driver (required for graphical session)
