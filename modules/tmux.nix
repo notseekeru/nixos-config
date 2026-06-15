567a115a058f3be3357b26c3de81278eca4fb563
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-15 19:22:55 +0800

    feat: extended keys csi for pi agent

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -7,31 +7,27 @@
     mouse = true;
 
     extraConfig = ''
-      set -g extended-keys on
-
       set -g mouse on
       set -s copy-command 'wl-copy'
       set -s set-clipboard on
 
-      # Start window numbering at 1 instead of 0
+      set -g extended-keys-format csi-u
+      set -g extended-keys on
+
       set -g base-index 1
       setw -g pane-base-index 1
 
-      # Only show windows 1-9
       set -g renumber-windows on
 
-      # Custom key bindings
       unbind-key C-o
       bind-key m select-pane -t :.+
 
-      # TPM plugins (loaded at runtime)
       set -g @plugin 'tmux-plugins/tpm'
       set -g @plugin 'tmux-plugins/tmux-sensible'
       set -g @plugin 'tmux-plugins/tmux-resurrect'
       set -g @plugin 'tmux-plugins/tmux-continuum'
       set -g @continuum-restore 'on'
 
-      # Initialize TPM
       run '~/.tmux/plugins/tpm/tpm'
     '';
 
