7ab69839fa16c0a6c9c56346b77f801462b30bdf
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-16 11:59:08 +0800

    fix: git token non persistent

--- modules/git.nix
+++ modules/git.nix
@@ -4,17 +4,19 @@
   programs.git = {
     enable = true;
 
+    # Use Home Manager's official options for user info
+    userName = "Stephen Macabulos";
+    userEmail = "myteseeker18@gmail.com";
+
     extraConfig = {
-      credential.helper = "cache"; # Or remove this if already set
-    };
+      # 1. Use the gh CLI as your password manager for GitHub
+      credential."https://github.com".helper = "!" + "${pkgs.gh}/bin/gh auth git-credential";
 
-    settings = {
-      user.name = "Stephen Macabulos";
-      user.email = "myteseeker18@gmail.com";
-
+      # 2. Your custom settings moved to the correct spot
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = true;
     };
   };
 }
+
