dda0ce09c15a04ec4fc8d5b4d859b89cca3c9d32
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-17 15:57:05 +0800

    feat: tmux auto restore

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,40 +1,34 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
 
-{
   programs.tmux = {
     enable = true;
-
     mouse = true;
+    keyMode = "vi";
 
-    extraConfig = ''
-      set -g mouse on
-      set -s copy-command 'wl-copy'
-      set -s set-clipboard on
-
-      set -g extended-keys-format csi-u
-      set -g extended-keys on
-
-      set -g base-index 1
-      setw -g pane-base-index 1
-
-      set -g renumber-windows on
-
-      unbind-key C-o
-      bind-key m select-pane -t :.+
-
-      set -g @plugin 'tmux-plugins/tpm'
-      set -g @plugin 'tmux-plugins/tmux-sensible'
-      set -g @plugin 'tmux-plugins/tmux-resurrect'
-      set -g @plugin 'tmux-plugins/tmux-continuum'
-      set -g @continuum-restore 'on'
-
-      run '~/.tmux/plugins/tpm/tpm'
+    extraConfig = '' 
+      set -g mouse on 
+      set -s copy-command 'wl-copy' 
+      set -s set-clipboard on 
+      set -g extended-keys-format csi-u 
+      set -g extended-keys on 
+      set -g base-index 1 
+      setw -g pane-base-index 1 
+      set -g renumber-windows on 
+      unbind-key C-o 
+      bind-key m select-pane -t :.+ 
     '';
 
     plugins = with pkgs.tmuxPlugins; [
       sensible
       resurrect
-      continuum
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '10'
+        '';
+      }
     ];
   };
 }
+
