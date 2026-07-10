a3de463c6075d168195f83a7bfb4629f9020ca77
Stephen Macabulos <myteseeker18@gmail.com>
2026-07-10 11:20:43 +0800

    feat(tmux): extra tmux 256colors

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -5,17 +5,19 @@
     mouse = true;
     keyMode = "vi";
 
-    extraConfig = '' 
-      set -g mouse on 
-      set -s copy-command 'wl-copy' 
-      set -s set-clipboard on 
-      set -g extended-keys-format csi-u 
-      set -g extended-keys on 
-      set -g base-index 1 
-      setw -g pane-base-index 1 
-      set -g renumber-windows on 
-      unbind-key C-o 
-      bind-key m select-pane -t :.+ 
+    extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -g mouse on
+      set -s copy-command 'wl-copy'
+      set -s set-clipboard on
+      set -g extended-keys-format csi-u
+      set -g extended-keys on
+      set -g base-index 1
+      setw -g pane-base-index 1
+      set -g renumber-windows on
+      unbind-key C-o
+      bind-key m select-pane -t :.+
 
       # Reproducible layout handled by tmux-init-sessions
     '';
